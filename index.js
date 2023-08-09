7cf1571fa03df53b165cc82c10477cc7d0305e96
Patryk <me@parpok.xyz>
2023-08-09 16:06:24 +0200

    Podmieniono rzecz w JS by wszystko dzialalo prawidlowo

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
-const liczba1 = document.getElementById('num1');
-const liczba2 = document.getElementById('num2');
-const l1 = parseFloat(liczba1.value);
-const l2 = parseFloat(liczba2.value);
-
     function dodawanie(){
+    const liczba1 = document.getElementById('num1');
+    const liczba2 = document.getElementById('num2');
+    const l1 = parseFloat(liczba1.value);
+    const l2 = parseFloat(liczba2.value);
+
         const result = l1 + l2;
         
         console.log("click " + result)
         return  document.getElementById("wynik").innerHTML = result;
     }
     function odejmowanie(){
+    const liczba1 = document.getElementById('num1');
+    const liczba2 = document.getElementById('num2');
+    const l1 = parseFloat(liczba1.value);
+    const l2 = parseFloat(liczba2.value);
+
         const result = l1 - l2;
         
         console.log("click" + result);
         return document.getElementById("wynik").innerHTML = result;
     }
     function mnozenie(){
+    const liczba1 = document.getElementById('num1');
+    const liczba2 = document.getElementById('num2');
+    const l1 = parseFloat(liczba1.value);
+    const l2 = parseFloat(liczba2.value);
+        
+
         const result = l1 * l2;
         
         console.log("click " + result)
         return document.getElementById("wynik").innerHTML = result;
     }
     function dzielenie(){
+    const liczba1 = document.getElementById('num1');
+    const liczba2 = document.getElementById('num2');
+    const l1 = parseFloat(liczba1.value);
+    const l2 = parseFloat(liczba2.value);
+
+
         const result = l1 / l2;
 
         console.log("click " + result)
@@ -34,3 +51,4 @@ const odejmij = document.getElementById('odejmij')?.addEventListener('click', od
 const pomnoz = document.getElementById('pomnoz')?.addEventListener('click', mnozenie);
 const podziel = document.getElementById('podziel')?.addEventListener('click', dzielenie);
 
+// all of that so JS it will work nice and will see changes in HTML
